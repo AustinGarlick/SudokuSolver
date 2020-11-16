1d690b45be662d165256d674f9738a19a13d3727
Austin <austingarlick@gmail.com>
2020-11-15 18:07:13 -0700

    Initial Commit

---
+++ SudokuSolver/src/SudokuSolver/SudokuSolver.java
@@ -0,0 +1,18 @@
+package SudokuSolver;
+
+
+
+
+/*
+ * This program solves a Sudoku Puzzle
+ * given the user's change to the global grid variable
+ */
+public class SudokuSolver {
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("hello");
+	}
+
+}
